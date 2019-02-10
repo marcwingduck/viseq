d8be8921a65bd6f7059496604ecc6b73a028ac61
Marc Lieser <marc.lieser@hs-rm.de>
2019-02-10 17:47:14 +0100

    Major Improvements and Fixes
    Initializing colors with current pot value.
    Normalizing amplitudes in log scale.
    Fixed issue where some bars peaked in silence which was caused by unlimited scaling.
    Switching between one, two and four column bin mode.
    When combining values into columns the max value is used instead of the average.

--- spectrum_analyzer/spectrum_analyzer.ino
+++ spectrum_analyzer/spectrum_analyzer.ino
@@ -1,85 +1,64 @@
-// led matrix //////////////////////////////////////////////////////////////////
-
 #include <Adafruit_GFX.h>   // adafruit graphics library
 #include <RGBmatrixPanel.h> // hardware-specific library
+#include <fix_fft.h>        // fft
 
-RGBmatrixPanel matrix(A0, A1, A2, 8, A3, 9, false);
-
-const int ledCols = 32;
-const int ledRows = 16;
-
-// colors
-const uint16_t off = matrix.Color444(0, 0, 0);
-uint16_t color1 = matrix.Color444(0, 2, 2);
-uint16_t color2 = matrix.Color444(0, 3, 1);
-
-// frame
-int frame = 0;
-
-// bin cols
-int binSize = 2;
-
-// peak fall off
-const int fallOff = 500;
-
-double scale = 1.;
-long int averageMillis = 0;
-
-// microphone //////////////////////////////////////////////////////////////////
-
+// pins
 #define MIC_PIN A5
-
-int micVal;
-
-// fft /////////////////////////////////////////////////////////////////////////
-
-#include <fix_fft.h>
-
-#define M  6
-#define N (1<<M)
-
-char re[64];
-char im[64];
-
-char spectrum[32];
-char peaks[32];
-long int peakMillis[32];
-
-int amplitude;
-int maxAmplitude = 1;
-
-// button //////////////////////////////////////////////////////////////////////
-
 #define BUTTON_PIN 13
-
-bool buttonVal = HIGH;
-bool prevButtonVal = HIGH;
-
-long int downTime = 0;
-
-// pot /////////////////////////////////////////////////////////////////////////
-
 #define POT_PIN A4
 
-int potVal = 0;
+// matrix size
+#define LED_COLS 32
+#define LED_ROWS 16
 
-// general /////////////////////////////////////////////////////////////////////
+// matrix and colors
+RGBmatrixPanel matrix(A0, A1, A2, 8, A3, 9, false);
+const uint16_t color0 = matrix.Color444(0, 0, 0);
+uint16_t color1 = color0;
+uint16_t color2 = color0;
 
-unsigned int i;
-unsigned int j;
+// fft
+#define M  6
+#define N (1<<M) // 64
+char re[N];
+char im[N];
+
+// N/2 must match LED_COLS!
+float spectrum[N / 2];
+char peaks[N / 2];
+long int peakMillis[N / 2];
+
+// dynamic scaling
+double scale = 1.;
+long int averageMillis = 0;
+float maxAmpNorm = 0.;
+float normAmp = log(32768);
+
+// button
+bool buttonVal = HIGH;
+bool prevButtonVal = HIGH;
+long int pushTime = 0;
+
+// counter
+unsigned int i = 0;
+unsigned int j = 0;
+
+// visualization settings
+int binSize = 1; // two column bin mode
+const int fallOff = 500; // peak fall off in ms
 
 // /////////////////////////////////////////////////////////////////////////////
 
 void setup()
 {
     analogReference(EXTERNAL);
-
     pinMode(BUTTON_PIN, INPUT);
     digitalWrite(BUTTON_PIN, HIGH); // enable pull-up
 
     matrix.begin();
-    matrix.fillScreen(off); // clear
 
+    // read initial colors
+    potLoop();
     resetPeaks();
 
     Serial.begin(9600);
@@ -87,13 +66,14 @@ void setup()
 
 void resetPeaks()
 {
-    char start = ledRows;
+    char start = LED_ROWS;
 
-    for (i = 0; i < ledCols; i++)
+    matrix.fillScreen(color0); // clear
+    for (i = 0; i < LED_COLS; i++)
     {
         peaks[i] = start;
         peakMillis[i] = millis();
-        matrix.drawPixel(i, ledRows - start, color2);
+        matrix.drawPixel(i, LED_ROWS - start, color2);
         delay(13);
     }
 }
@@ -108,20 +88,22 @@ void loop()
     potLoop();
 
     // consolidate bins
-    if (binSize > 1)
+    if (binSize > 0)
     {
-        for (i = 0; i < ledCols / binSize; i++)
-        {
-            int avg = 0;
-            for (j = 0; j < binSize; j++)
-            {
-                avg += spectrum[i * binSize + j];
-            }
-            avg /= binSize;
+        int binWidth = 1 << binSize;
 
-            for (j = 0; j < binSize; j++)
+        for (i = 0; i < LED_COLS / binWidth; i++)
+        {
+            float maxVal = 0.;
+            for (j = 0; j < binWidth; j++)
             {
-                spectrum[i * binSize + j] = avg;
+                float val = spectrum[i * binWidth + j];
+                maxVal = max(maxVal, val);
+            }
+
+            for (j = 0; j < binWidth; j++)
+            {
+                spectrum[i * binWidth + j] = maxVal;
             }
         }
     }
@@ -132,12 +114,13 @@ void loop()
     if (millis() - averageMillis > 1000)
     {
         averageMillis = millis();
-        scale = ledRows / maxAmplitude;
-        maxAmplitude = 1;
+        scale = 1.;
+        if (maxAmpNorm > 0.16) // silence returns values of up to 0.15
+        {
+            scale = (1. / maxAmpNorm) * LED_ROWS;
+        }
+        maxAmpNorm = 0.;
     }
-
-    // progress frame
-    frame = (frame + 1) % ledCols;
 }
 
 // /////////////////////////////////////////////////////////////////////////////
@@ -147,10 +130,8 @@ void micLoop()
     // collect mic signals
     for (i = 0; i < N; i++)
     {
-        // read DAC [0,1023]
-        micVal = analogRead(MIC_PIN);
-        // convert to char range
-        re[i] = (micVal / 4) - 128; // [-128,127]
+        // read DAC map from [0,1023] to [-128,127]
+        re[i] = analogRead(MIC_PIN) / 4 - 128;
         // set imaginary part to zero
         im[i] = 0;
     }
@@ -164,33 +145,26 @@ void fftLoop()
     // calculate amplitudes
     for (i = 0; i < N / 2; i++)
     {
-        amplitude = re[i] * re[i] + im[i] * im[i]; // max 32768
+        spectrum[i] = 0.;
 
-        if (amplitude == 0)
+        int amp = re[i] * re[i] + im[i] * im[i]; // max 32768
+
+        if (amp > 0)
         {
-            spectrum[i] = 0;
-            continue;
+            // log scale [0., 10.3972] normalized to [0.,1.]
+            float ampNorm = log(amp) / normAmp;
+            maxAmpNorm = max(maxAmpNorm, ampNorm);
+            spectrum[i] = ampNorm;
         }
-
-        // log scale [0,10]
-        amplitude = log(amplitude);
-
-        if (amplitude > maxAmplitude)
-        {
-            maxAmplitude = amplitude;
-        }
-
-        spectrum[i] = amplitude;
     }
 }
 
 void potLoop()
 {
     // read DAC [0,1023]
-    potVal = analogRead(POT_PIN);
+    int potVal = analogRead(POT_PIN);
 
     long hue1 = map(potVal, 0, 1023, 0, 1535);
-
     long hue2 = hue1 - 200;
     if (hue2 < 0)
     {
@@ -207,17 +181,17 @@ void buttonLoop()
 
     if (prevButtonVal == HIGH && buttonVal == LOW) // got pushed
     {
-        downTime = millis();
+        pushTime = millis();
     }
     else if (prevButtonVal == LOW && buttonVal == HIGH) // got released
     {
-        if (millis() - downTime > 500)
-        {
-            binSize = binSize == 1 ? 2 : 1;
-        }
-        else
+        if (millis() - pushTime > 500) // long push
         {
             resetPeaks();
+        }
+        else // short push
+        {
+            binSize = (binSize + 1) % 3;
         }
     }
 
@@ -226,13 +200,10 @@ void buttonLoop()
 
 void drawLoop()
 {
-    for (i = 0; i < ledCols; i++)
+    for (i = 0; i < LED_COLS; i++)
     {
-        char val = spectrum[i] * scale;
-        if (val > ledRows)
-        {
-            val = ledRows;
-        }
+        char val = round(spectrum[i] * scale);
+        val = min(val, LED_ROWS);
 
         if (val > peaks[i])
         {
@@ -246,15 +217,15 @@ void drawLoop()
         }
 
         // clear screen and draw bins
-        for (j = 0; j < ledRows; j++)
+        for (j = 0; j < LED_ROWS; j++)
         {
-            matrix.drawPixel(i, ledRows - 1 - j, j < val ? color1 : off);
+            matrix.drawPixel(i, LED_ROWS - 1 - j, j < val ? color1 : color0);
         }
 
         // draw peaks
         if (peaks[i] > 0)
         {
-            matrix.drawPixel(i, ledRows - peaks[i], color2);
+            matrix.drawPixel(i, LED_ROWS - peaks[i], color2);
         }
     }
 }
